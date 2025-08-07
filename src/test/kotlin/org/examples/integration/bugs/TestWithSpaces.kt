2465ffa87a9d1b5b772fdc4400bbb64cfc62d819
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-08-07 13:37:38 +0200

    Add a new test with spaces in the name

---
+++ src/test/kotlin/org/examples/integration/bugs/TestWithSpaces.kt
@@ -0,0 +1,10 @@
+package org.examples.integration.bugs
+
+import org.junit.jupiter.api.Test
+
+class TestWithSpaces {
+    @Test
+    fun `test with space`() {
+        // This test should be checked in both modes: Gradle and JUnit
+    }
+}

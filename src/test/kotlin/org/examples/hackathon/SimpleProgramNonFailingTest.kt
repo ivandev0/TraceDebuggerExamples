9ed47724f4dcfe35fc784279e7d91e5175443299
Nikita Koval <ndkoval@ya.ru>
2025-09-17 17:53:19 +0200

    Add a new test

---
+++ src/test/kotlin/org/examples/hackathon/SimpleProgramNonFailingTest.kt
@@ -0,0 +1,25 @@
+package org.examples.hackathon
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SimpleProgramNonFailingTest {
+    @Test
+    fun test() {
+        val numbers = listOf(1, 2, 3)
+        val sum = sum(numbers)
+        try {
+            assertEquals(6, sum)
+        } catch (e: Exception) {
+            // Ignore
+        }
+    }
+}
+
+private fun sum(numbers: List<Int>): Int {
+    var sum = 0
+    for (i in 0 until numbers.size - 1) {
+        sum += numbers[i]
+    }
+    return sum
+}

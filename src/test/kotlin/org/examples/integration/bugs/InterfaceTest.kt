a998ff48ee45305ad64b851744b290a2500ff33c
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2025-11-03 23:28:54 +0100

    Add a reflection test

---
+++ src/test/kotlin/org/examples/integration/bugs/InterfaceTest.kt
@@ -0,0 +1,52 @@
+package org.examples.integration.bugs
+
+import org.junit.jupiter.api.Test
+
+interface Interface {
+    fun method()
+}
+
+class InterfaceImpl : Interface {
+    private fun f() {}
+    override fun method() {
+        f()
+    }
+}
+
+val classLoader: ClassLoader = Interface::class.java.classLoader
+
+class MyClassLoader : ClassLoader(classLoader) {
+    public override fun loadClass(name: String, resolve: Boolean): Class<*> {
+        return super.loadClass(name, resolve)
+    }
+
+    public override fun findClass(moduleName: String, name: String): Class<*> {
+        return super.findClass(moduleName, name)
+    }
+
+    override fun loadClass(name: String): Class<*> {
+        return super.loadClass(name)
+    }
+}
+
+
+const val className = "org.examples.integration.bugs.InterfaceImpl"
+fun getInterface1(): Interface = Class.forName(className).getDeclaredConstructor().newInstance() as Interface
+fun getInterface2(): Interface = classLoader.loadClass(className).getDeclaredConstructor().newInstance() as Interface
+fun getInterface3(): Interface = MyClassLoader().loadClass(className).getDeclaredConstructor().newInstance() as Interface
+fun getInterface4(): Interface = MyClassLoader().loadClass(className, true).getDeclaredConstructor().newInstance() as Interface
+
+
+class InterfaceTest {
+    @Test
+    fun test() {
+        val i1 = getInterface1()
+        i1.method()
+        val i2 = getInterface2()
+        i2.method()
+        val i3 = getInterface3()
+        i3.method()
+        val i4 = getInterface4()
+        i4.method()
+    }
+}
